fbaeb95011d99ee5ee03dac5219ddd1d1b0655e2
RasmusGunnar <rgjconsult@gmail.com>
2025-11-01 16:08:47 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,6 +0,0 @@
-// Default Firebase konfiguration. Kopiér indholdet fra firebase-config.example.js og
-// indsæt virksomhedens egne nøgler før deployment.
-window.SUBRA_FIREBASE_CONFIG = null;
-window.SUBRA_KIOSK_COLLECTION = 'kiosks';
-window.SUBRA_KIOSK_DOCUMENT = 'subra-main';
-window.SUBRA_ASSET_FOLDER = 'screensaver';
