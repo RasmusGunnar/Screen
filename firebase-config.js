16558c9e2bfc2c61c57a68900825e0f56924a7e8
RasmusGunnar <rgjconsult@gmail.com>
2025-11-18 14:54:00 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
-// Firebase Web Config (må gerne ligge offentligt i repoet)
+
 window.SUBRA_FIREBASE_CONFIG = {
   apiKey: "AIzaSyA_gh8y-V6kkl95hRzb0ldBxpKWFY5bAck",
   authDomain: "webkiosk-e5d32.firebaseapp.com",
   projectId: "webkiosk-e5d32",
-  storageBucket: "webkiosk-e5d32.appspot.com", // ← rettet fra firebasestorage.app
+  storageBucket: "webkiosk-e5d32.firebasestorage.app",
   messagingSenderId: "920175197353",
-  appId: "1:920175197353:web:6d8777eb15e352c1c4fdb8",
-  measurementId: "G-MPYQYDEE62" // valgfri (kun hvis du bruger Analytics)
+  appId: "1:920175197353:web:c3520e9a3ab8146ec4fdb8",
+  measurementId: "G-X6LHDEPS8H"
 };
